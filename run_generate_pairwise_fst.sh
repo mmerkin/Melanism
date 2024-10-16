9ffdeadcf6b9e3cc4753e85c8c200a593f818326
mmerkin <163770255+mmerkin@users.noreply.github.com>
2024-10-16 19:40:52 +0100

    Update run_generate_pairwise_fst.sh

--- run_generate_pairwise_fst.sh
+++ run_generate_pairwise_fst.sh
@@ -1,56 +1,77 @@
 #! /bin/bash
 
+#Prerequisites: vcftools installed, population sample files created in the format SPECIESID_POP_samples.txt 
+
+
 # Set variables
 
-# Full path to gzipped vcf
-gzvcf_path=~/final_vcfs/Bb_fb_background.vcf.gz 
-# Designate species for output
-species=Bb
+# Full path to .vcf or .vcf.gz file
+VCF=/path/to/variants.vcf.gz 
+# Designate species ID for output names
+SPECIESID=example
 # Space or tab separated list of population names
-Pop_list=(BTW CDY HTH KKH MDP OGR WRT WBP)
+POPLIST=(POP1 POP2 POP3 POP4)
+
+
+
+
+
+
+
 
 # Run code
 
-# Generate a vcf file for all pairwise combinations of populations. This does include repeats, so filter those out later
+# Generate a vcf file for all pairwise combinations of populations after determining whether the vcf is gzipped
 
-for i in "${Pop_list[@]}"; do 
-	for j in "${Pop_list[@]}"; do 
-		if [ $i = $j ]; then
-			continue
-		fi
-		vcftools --gzvcf ${gzvcf_path} --weir-fst-pop ${species}_${i}_samples.txt --weir-fst-pop ${species}_${j}_samples.txt --out ${species}_${i}_vs_${j}
+if [[ $VCF =~ \.gz$ ]]; then
+	for i in "${POPLISTt[@]}"; do 
+		for j in "${POPLIST[@]}"; do 
+			if [ $i = $j ]; then
+				continue
+			fi
+			vcftools --gzvcf ${VCF} --weir-fst-pop ${SPECIESID}_${i}_samples.txt --weir-fst-pop ${SPECIESID}_${j}_samples.txt --out ${SPECIESID}_${i}_vs_${j}
+		done
 	done
-done
+else
+    for i in "${POPLIST[@]}"; do 
+		for j in "${POPLIST[@]}"; do 
+			if [ $i = $j ]; then
+				continue
+			fi
+			vcftools --vcf ${VCF} --weir-fst-pop ${SPECIESID}_${i}_samples.txt --weir-fst-pop ${SPECIESID}_${j}_samples.txt --out ${SPECIESID}_${i}_vs_${j}
+		done
+	done
+fi
 
 # Generate the names column
 
-for i in "${Pop_list[@]}"; do 
-	for j in "${Pop_list[@]}"; do 
+for i in "${POPLIST[@]}"; do 
+	for j in "${POPLIST[@]}"; do 
 		if [ $i = $j ]; then
 			continue
 		fi
 		echo "${i}-${j}"
 	done
-done > ${species}_pairwise_comparisons.txt
+done > ${SPECIESID}_pairwise_comparisons.txt
 
 # Generate the mean FST column
 
-for i in "${Pop_list[@]}"; do 
-	for j in "${Pop_list[@]}"; do 
+for i in "${POPLIST[@]}"; do 
+	for j in "${POPLIST[@]}"; do 
 		if [ $i = $j ]; then
 			continue
 		fi
-		grep 'Weir and Cockerham mean Fst estimate:' ${species}_${i}_vs_${j}.log | sed 's/^.*: //'
+		grep 'Weir and Cockerham mean Fst estimate:' ${SPECIESID}_${i}_vs_${j}.log | sed 's/^.*: //'
 	done
-done > ${species}_mean_fsts.txt
+done > ${SPECIESID}_mean_fsts.txt
 
 # Generate the weighted FST column
 
-for i in "${Pop_list[@]}"; do 
-	for j in "${Pop_list[@]}"; do 
+for i in "${POPLIST[@]}"; do 
+	for j in "${POPLIST[@]}"; do 
 		if [ $i = $j ]; then
 			continue
 		fi
-		grep 'Weir and Cockerham weighted Fst estimate:' ${species}_${i}_vs_${j}.log | sed 's/^.*: //'
+		grep 'Weir and Cockerham weighted Fst estimate:' ${SPECIESID}_${i}_vs_${j}.log | sed 's/^.*: //'
 	done
-done > ${species}_weighted_fsts.txt
+done > ${SPECIESID}_weighted_fsts.txt
