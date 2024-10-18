b2166eb5e4ca28818d46bf93761f8343a9c1c280
mmerkin <163770255+mmerkin@users.noreply.github.com>
2024-10-18 15:41:05 +0100

    Create run_map_reads.sh

---
+++ run_map_reads.sh
@@ -0,0 +1,54 @@
+
+# Map, filter and index reads
+
+#!/bin/bash
+
+# Requirements: bwa-mem2
+
+# Reference genome
+REF=/path/to/reference.fa
+# Short reads
+datapath=/path/to/reads.fastq
+# file to place final bams
+output=/path/to/output/file
+# Number of threads to use
+threads=32
+# Change to false to keep temporary files
+remove_temp=true
+
+
+
+
+
+# Code
+
+mkdir -p $output
+
+for file in "$datapath"/*; do 
+filetag=${file##*/}
+
+echo "Mapping reads"
+bwa-mem2 mem -t 32 $REF $file/*R1*.fastq.gz $file/*R2*.fastq.gz > "$output/${filetag}.raw.bam"
+
+echo "Sorting by name"
+samtools sort -@ $threads -n "$output/${filetag}.filtered.bam" -o "$output/${filetag}.sorted.n.bam"
+
+# Fixmate -r filters the reads and -m adds a mate score tag for markdup to select the best reads to keep
+
+echo "Removing unmapped and secondary reads"
+samtools fixmate -rm "$output/${filetag}.sorted.n.bam" "$output/${filetag}.fixmate.bam"
+
+echo "Sorting by position"
+samtools sort -@ $threads "$output/${filetag}.fixmate.bam" -o "$output/${filetag}.sorted.p.bam"
+
+echo "Marking duplicates"
+samtools markdup -r -@ $threads "$output/${filetag}.sorted.p.bam" "$output/${filetag}.bam"
+echo "running index"
+samtools index "$output/${filetag}.bam"
+
+if $remove_temp; then
+  rm "$output/${filetag}.raw.bam" "$output/${filetag}.sorted.n.bam" "$output/${filetag}.fixmate.bam" "$output/${filetag}.sorted.p.bam"
+fi
+
+done
+
