c6a7263d659b637142fb70c35e1971a1b865774f
mmerkin <163770255+mmerkin@users.noreply.github.com>
2024-10-22 11:07:47 +0100

    Update run_generate_depths.sh

--- scripts/run_generate_depths.sh
+++ scripts/run_generate_depths.sh
@@ -16,11 +16,10 @@ first=true
 for file in "$datapath"/*."$filetype"; do 
 if $first; then
         echo -e "Sample_id\tAverage_depth"
-else
-        filebase=$(basename "$file" ".$filetype")
-        filetag=${filebase##*/}
-        depth=$(samtools depth "$file"  | awk '{sum+=$3} END { print sum/NR}')
-        echo -e "${filetag}\t${depth}"
+        first=false
 fi
-first=false   
+filebase=$(basename "$file" ".$filetype")
+filetag=${filebase##*/}
+depth=$(samtools depth "$file"  | awk '{sum+=$3} END { print sum/NR}')
+echo -e "${filetag}\t${depth}"
 done > $output
