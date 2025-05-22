15e75047ccfbdc28cfb46e8fcfbc9ceee7b977ac
mmerkin <163770255+mmerkin@users.noreply.github.com>
2025-05-22 16:26:59 +0100

    Update run_map_reads.sh

--- scripts/run_map_reads.sh
+++ scripts/run_map_reads.sh
@@ -25,6 +25,7 @@ for file in "$datapath"/*; do
 filetag=${file##*/}
 
 echo "Mapping reads"
+
 bwa-mem2 mem -t $threads $REF $file/*R1*.fastq.gz $file/*R2*.fastq.gz > "$output/${filetag}.raw.bam"
 
 echo "Filtering reads"
@@ -32,19 +33,25 @@ echo "Filtering reads"
 samtools view -@ $threads -b -F 2828 -q 20 "$output/$filetag/$filetag.raw.bam" -o "$output/$filetag/$filetag.filtered.bam"
 
 echo "Sorting by name"
+
 samtools sort -@ $threads -n "$output/${filetag}.filtered.bam" -o "$output/${filetag}.sorted.n.bam"
 
 # Fixmate -r filters the reads and -m adds a mate score tag for markdup to select the best reads to keep
 
 echo "Adding fixmate tags"
+
 samtools fixmate -rm -@ $threads "$output/${filetag}.sorted.n.bam" "$output/${filetag}.fixmate.bam"
 
 echo "Sorting by position"
+
 samtools sort -@ $threads "$output/${filetag}.fixmate.bam" -o "$output/${filetag}.sorted.p.bam"
 
 echo "Marking duplicates"
+
 samtools markdup -r -@ $threads "$output/${filetag}.sorted.p.bam" "$output/${filetag}.bam"
+
 echo "running index"
+
 samtools index "$output/${filetag}.bam"
 
 if $remove_temp; then
